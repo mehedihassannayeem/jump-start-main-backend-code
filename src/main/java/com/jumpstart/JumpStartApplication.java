16a29d0af940292f6f1a1c609d20446562df0831
mehedi-hassan <rawedhassan17@gmail.com>
2023-03-05 08:19:14 +0600

    base code

---
+++ src/main/java/com/jumpstart/JumpStartApplication.java
@@ -0,0 +1,13 @@
+package com.jumpstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JumpStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JumpStartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jumpstart/JumpStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jumpstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JumpStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
